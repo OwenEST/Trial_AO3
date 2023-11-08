9b294846e5cf173feb1d2aa4e1025210c85e83b9
OwenEST <Owen.constantine@cydeo.com>
2023-11-08 20:12:24 +0300

    First commit with Hello

--- src/test/java/com/project_name/trial/Conflict.java
+++ src/test/java/com/project_name/trial/Conflict.java
@@ -3,5 +3,6 @@ package com.project_name.trial;
 public class Conflict {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("conflict_O");
     }
 }
