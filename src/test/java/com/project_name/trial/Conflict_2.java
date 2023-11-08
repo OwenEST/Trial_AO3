180f7278761a1371d360ef9b38a560a8bce23f20
CrazyAlvin <alvinalvares.it@gmail.com>
2023-11-08 14:28:40 -0500

    firs commit

--- src/test/java/com/project_name/trial/Conflict_2.java
+++ src/test/java/com/project_name/trial/Conflict_2.java
@@ -2,6 +2,6 @@ package com.project_name.trial;
 
 public class Conflict_2 {
     public static void main(String[] args) {
-        System.out.println("conflict O");
+        System.out.println("Conflict 1");
     }
 }
